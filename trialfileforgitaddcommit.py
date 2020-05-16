e69b9efa39d25d927a7134a14cc82d24f7b0820b
Jamison Higa <65368346+jhigaLI@users.noreply.github.com>
2020-05-16 11:19:01 -0400

    Update trialfileforgitaddcommit.py

--- trialfileforgitaddcommit.py
+++ trialfileforgitaddcommit.py
@@ -7,3 +7,14 @@ and then confirm I can also use git pull to then
 have these changes realized on my local branch.
 
 """
+
+"""
+
+The trial was a success! My other file, "testing gitadd..."
+was also added. This gives me confidence in adding more than
+one file at a time for commitment. This second paragraph
+was edited from the origin remote (GitHub site). This file
+will now be pulled from the origin to the local branch. This
+is great!
+
+"""

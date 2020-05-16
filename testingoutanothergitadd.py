4e83ea0275ab1cb8512749c063dacb27785c83bd
Jamison Higa <jamison.higa@gmail.com>
2020-05-16 11:15:00 -0400

    These are sample files I'm using to test my git add/commit/etc.. knowledge

---
+++ testingoutanothergitadd.py
@@ -0,0 +1,16 @@
+"""
+
+I know that "git add <file name>" adds the
+exact file for commitment. Does "git add ."
+add all untracked files for commitment?
+
+"""
+
+"""
+
+Yes, yes it does!
+
+Now I will see what happens when two files
+are committed at the same time.
+
+"""
---
+++ trialfileforgitaddcommit.py
@@ -0,0 +1,9 @@
+"""
+
+I am using this file to solidfy my understanding
+of git add/commit/push commands. I will then
+edit this file on the origin remote (GitHub site)
+and then confirm I can also use git pull to then
+have these changes realized on my local branch.
+
+"""
